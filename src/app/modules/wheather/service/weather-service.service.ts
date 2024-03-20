ed4f0cf67ea334f6d39b0d760ae80de3fe8331de
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-03-20 00:19:20 -0300

    Feat: method for get weather datas

--- src/app/modules/wheather/service/weather-service.service.ts
+++ src/app/modules/wheather/service/weather-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherServiceService {
+  private apiKey = '2ad90ed5fc16cde0674b8e2be9186c80';
 
-  constructor() { }
+  constructor(private Http: HttpClient) {}
+
+  getWeaterDatas(cityName: string): Observable<any> {
+    return this.Http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
 }
