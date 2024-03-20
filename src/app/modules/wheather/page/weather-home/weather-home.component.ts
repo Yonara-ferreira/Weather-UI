13e84714b17f748eda110c8c05a43aff7f2858cc
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-03-20 00:40:21 -0300

    Feat: Production method initializer

--- src/app/modules/wheather/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherServiceService } from '../../service/weather-service.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
 
+  initialCityName = 'Fortaleza';
+
+
+  constructor(private weatherService: WeatherServiceService) {}
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeaterDatas(cityName).subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: (error: any) => console.log(error),
+    });
+  }
 }
