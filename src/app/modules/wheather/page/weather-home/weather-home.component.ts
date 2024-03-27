643da92fd08cc55a67df6aa85fcb1bd331ef0bb4
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-03-26 22:57:00 -0300

    Feat: Implemented search form

--- src/app/modules/wheather/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheather/page/weather-home/weather-home.component.ts
@@ -1,29 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherServiceService } from '../../service/weather-service.service';
 import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
-
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName = 'Fortaleza';
   weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherServiceService) {}
+
   ngOnInit(): void {
     this.getWeatherDatas(this.initialCityName);
   }
 
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName).subscribe({
-      next: (response: any) => {
-        response && (this.weatherDatas = response);
-        console.log(this.weatherDatas);
-      },
-      error: (error: any) => console.log(error),
-    });
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response: any) => {
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas);
+        },
+        error: (error: any) => console.log(error),
+      });
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
